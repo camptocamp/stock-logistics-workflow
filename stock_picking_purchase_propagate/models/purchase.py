cda0a619e48020f66852ad070dfc0a45e739e51b
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-05-18 09:27:37 +0200

    fixup! Improve performance using direct SQL

--- stock_picking_purchase_propagate/models/purchase.py
+++ stock_picking_purchase_propagate/models/purchase.py
@@ -15,22 +15,7 @@ class PurchaseOrderLine(models.Model):
             reassign pickings.
         """
         moves = super(PurchaseOrderLine, self)._create_stock_moves(picking)
-
-        sql = """
-            SELECT dest_mov.id
-            FROM stock_move dest_mov
-            INNER JOIN stock_move_move_rel rel
-                ON rel.move_dest_id = dest_mov.id
-            INNER JOIN stock_move orig_mov ON rel.move_orig_id = orig_mov.id
-            WHERE orig_mov.propagate = TRUE
-            AND dest_mov.group_id IS NULL
-            AND orig_mov.id IN %s;
-            """
-        self.env.cr.execute(sql, [tuple(moves.ids)])
-        dest_moves_ids = [x[0] for x in self.env.cr.fetchall()]
-        destination_moves_to_prop = self.env['stock.move'].browse(
-            dest_moves_ids)
-
+        destination_moves_to_prop = moves.get_next_moves_to_propagate()
         destination_moves_to_prop._propagate_procurement_group(
             moves.mapped('group_id'))
         moves._propagate_quantity_to_dest_moves()
