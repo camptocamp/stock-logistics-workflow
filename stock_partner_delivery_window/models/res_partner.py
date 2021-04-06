3057d79f6fc60cf72cc20593fb9b92cf48595410
Matthieu Méquignon <matthieu.mequignon@camptocamp.com>
2021-04-06 16:46:59 +0200

    Cleanup

--- stock_partner_delivery_window/models/res_partner.py
+++ stock_partner_delivery_window/models/res_partner.py
@@ -1,5 +1,6 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+from collections import defaultdict
 from datetime import time
 
 from odoo import _, api, fields, models
@@ -49,19 +50,13 @@ class ResPartner(models.Model):
         :param day: The day name (see time.weekday, ex: 0,1,2,...)
         :return: dict partner_id: delivery_window recordset
         """
-        res = {}
+        self.ensure_one()
         domain = [("partner_id", "in", self.ids)]
         if day_name is not None:
             week_day_id = self.env["time.weekday"]._get_id_by_name(day_name)
             domain.append(("time_window_weekday_ids", "in", week_day_id))
         windows = self.env["partner.delivery.time.window"].search(domain)
-        for window in windows:
-            if not res.get(window.partner_id.id):
-                res[window.partner_id.id] = self.env[
-                    "partner.delivery.time.window"
-                ].browse()
-            res[window.partner_id.id] |= window
-        return res
+        return windows
 
     def is_in_delivery_window(self, date_time):
         """
@@ -71,7 +66,7 @@ class ResPartner(models.Model):
         :return: Boolean
         """
         self.ensure_one()
-        windows = self.get_delivery_windows(date_time.weekday()).get(self.id)
+        windows = self.get_delivery_windows(date_time.weekday())
         if windows:
             for w in windows:
                 start_time = w.get_time_window_start_time()
@@ -87,52 +82,42 @@ class ResPartner(models.Model):
         return False
 
     def _get_delivery_time_format_string(self):
-        return _("From %s to %s")
+        return _("From {} to {}")
 
     def get_delivery_time_description(self):
         res = dict()
         day_translated_values = dict(
             self.env["time.weekday"]._fields["name"]._description_selection(self.env)
         )
+
+        def short_format_time(time):
+            return format_time(self.env, time, time_format="short")
+
+        weekdays = self.env["time.weekday"].search([])
         for partner in self:
-            opening_times = {}
+            opening_times = defaultdict(list)
             time_format_string = self._get_delivery_time_format_string()
             if partner.delivery_time_preference == "time_windows":
-                for day in self.env["time.weekday"].search([]):
+                for day in weekdays:
                     day_windows = partner.delivery_time_window_ids.filtered(
                         lambda d: day in d.time_window_weekday_ids
                     )
                     for win in day_windows:
-                        opening_times.setdefault(day_translated_values[day.name], [])
-                        opening_times[day_translated_values[day.name]].append(
-                            time_format_string
-                            % (
-                                format_time(
-                                    self.env,
-                                    win.get_time_window_start_time(),
-                                    time_format="short",
-                                ),
-                                format_time(
-                                    self.env,
-                                    win.get_time_window_end_time(),
-                                    time_format="short",
-                                ),
-                            )
+                        start = win.get_time_window_start_time()
+                        end = win.get_time_window_end_time()
+                        translated_day = day_translated_values[day.name]
+                        value = time_format_string.format(
+                            short_format_time(start), short_format_time(end),
                         )
+                        opening_times[translated_day].append(value)
             else:
-                for day in self.env["time.weekday"].search([]):
-                    opening_times.setdefault(day_translated_values[day.name], [])
-                    opening_times[day_translated_values[day.name]].append(
-                        time_format_string
-                        % (
-                            format_time(
-                                self.env, time(hour=0, minute=0), time_format="short"
-                            ),
-                            format_time(
-                                self.env, time(hour=23, minute=59), time_format="short"
-                            ),
-                        )
+                for day in weekdays:
+                    translated_day = day_translated_values[day.name]
+                    value = time_format_string.format(
+                        short_format_time(time(hour=0, minute=0)),
+                        short_format_time(time(hour=23, minute=59)),
                     )
+                    opening_times[translated_day].append(value)
             opening_times_description = list()
             for day_name, time_list in opening_times.items():
                 opening_times_description.append(
