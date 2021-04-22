4d46298e6e008ca3fc2907e555b36dbacfe50fa9
Sébastien Alix <sebastien.alix@camptocamp.com>
2021-04-22 09:10:58 +0200

    fixup! [IMP] s_p_g_by_p_by_carrier: disable grouping on partner

--- stock_picking_group_by_partner_by_carrier/models/stock_picking.py
+++ stock_picking_group_by_partner_by_carrier/models/stock_picking.py
@@ -59,13 +59,14 @@ class StockPicking(models.Model):
             return super().action_cancel()
 
     def _create_backorder(self):
-        if self.partner_id.disable_picking_grouping:
-            return super()._create_backorder()
-        if self.picking_type_id.group_pickings:
-            self = self.with_context(picking_no_copy_if_can_group=1)
-        backorders = super()._create_backorder()
-        if self.picking_type_id.group_pickings:
-            backorders._merge_procurement_groups()
+        backorders = self.browse()
+        for picking in self:
+            if not picking._is_grouping_disabled():
+                picking = picking.with_context(picking_no_copy_if_can_group=1)
+            backorder = super(StockPicking, picking)._create_backorder()
+            if not picking._is_grouping_disabled():
+                backorder._merge_procurement_groups()
+            backorders |= backorder
         return backorders
 
     def _prepare_merge_procurement_group_values(self, move_groups):
@@ -77,9 +78,7 @@ class StockPicking(models.Model):
 
     def _merge_procurement_groups(self):
         for picking in self:
-            if not picking.picking_type_id.group_pickings:
-                continue
-            if picking.partner_id.disable_picking_grouping:
+            if picking._is_grouping_disabled():
                 continue
             if picking.picking_type_id.code != "outgoing":
                 continue
@@ -118,6 +117,13 @@ class StockPicking(models.Model):
         return super(
             StockPicking, self.with_context(picking_no_copy_if_can_group=0)
         ).copy(defaults)
+
+    def _is_grouping_disabled(self):
+        self.ensure_one()
+        return (
+            not self.picking_type_id.group_pickings
+            or self.partner_id.disable_picking_grouping
+        )
 
     def get_delivery_report_lines(self):
         self.ensure_one()
