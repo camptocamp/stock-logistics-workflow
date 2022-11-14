74ad412ef5bf7a15d798df421642af748ff7700f
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2022-11-14 15:24:17 +0200

    stock_scheduler_assignation_horizon: wider settings approuche
    scheduler ususally running in global mode when company is not set
    so settings defined on company level are not applied

--- stock_scheduler_assignation_horizon/models/procurement_group.py
+++ stock_scheduler_assignation_horizon/models/procurement_group.py
@@ -15,18 +15,23 @@ class ProcurementGroup(models.Model):
     def _get_moves_to_assign_domain(self, company_id):
         domain = super()._get_moves_to_assign_domain(company_id)
         company = self.env["res.company"].browse(company_id)
-        if company.is_moves_assignation_limited:
+        settings = self.env["res.config.settings"]
+        global_limit = settings.get_global_is_moves_assignation_limited()
+        if company.is_moves_assignation_limited or global_limit:
+            delay = (
+                company.moves_assignation_horizon
+                or settings.get_global_moves_limit_horizon()
+            )
             domain = self._get_moves_to_assign_domain_assignation_limited(
-                domain, company
+                domain, company.partner_id.tz, delay
             )
         return domain
 
-    def _get_moves_to_assign_domain_assignation_limited(self, domain, company):
-        company_tz = pytz.timezone(company.partner_id.tz or "UTC")
+    def _get_moves_to_assign_domain_assignation_limited(self, domain, tz, delay_days):
+        applied_tz = pytz.timezone(tz or "UTC")
         max_date = (
             datetime.combine(
-                datetime.now(company_tz)
-                + timedelta(days=company.moves_assignation_horizon),
+                datetime.now(applied_tz) + timedelta(days=delay_days),
                 datetime.max.time(),
             )
             .astimezone(pytz.utc)
--- stock_scheduler_assignation_horizon/readme/DESCRIPTION.rst
+++ stock_scheduler_assignation_horizon/readme/DESCRIPTION.rst
@@ -1,1 +1,4 @@
-This module allows to set a time horizon that will prevent the scheduler from marking stock pickings as *ready* before the indicated number of days (horizon).
+This module allows to set a time horizon that will prevent the scheduler from marking stock
+pickings as *ready* before the indicated number of days (horizon).
+Options can be enabled globally and for each company separately, remember that to enable
+mode for the company scheduler needs to be modified to set the company correctly.
