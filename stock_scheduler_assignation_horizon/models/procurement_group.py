f5ad5af64fd3772d415a3cbfff03645bcae6d28e
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2022-11-14 15:24:17 +0200

    stock_scheduler_assignation_horizon: wider settings approach
    scheduler usually running in global mode when company is not set
    so settings defined on company level are not applied

--- stock_scheduler_assignation_horizon/models/procurement_group.py
+++ stock_scheduler_assignation_horizon/models/procurement_group.py
@@ -7,6 +7,8 @@ import pytz
 
 from odoo import models
 from odoo.osv import expression
+from odoo.tools.misc import str2bool
+from odoo.tools.safe_eval import safe_eval
 
 
 class ProcurementGroup(models.Model):
@@ -15,18 +17,23 @@ class ProcurementGroup(models.Model):
     def _get_moves_to_assign_domain(self, company_id):
         domain = super()._get_moves_to_assign_domain(company_id)
         company = self.env["res.company"].browse(company_id)
-        if company.is_moves_assignation_limited:
+        settings = self.env["res.config.settings"]
+        global_limit = settings.get_global_is_moves_assignation_limited()
+        if company.is_moves_assignation_limited or global_limit:
+            delay = (
+                company.moves_assignation_horizon
+                or settings.get_global_moves_limit_horizon()
+            )
             domain = self._get_moves_to_assign_domain_assignation_limited(
-                domain, company
+                domain, company.partner_id.tz, delay
             )
         return domain
 
-    def _get_moves_to_assign_domain_assignation_limited(self, domain, company):
-        company_tz = pytz.timezone(company.partner_id.tz or "UTC")
+    def _get_moves_to_assign_domain_assignation_limited(self, domain, tz, delay_days):
+        applied_tz = pytz.timezone(tz or "UTC")
         max_date = (
             datetime.combine(
-                datetime.now(company_tz)
-                + timedelta(days=company.moves_assignation_horizon),
+                datetime.now(applied_tz) + timedelta(days=delay_days),
                 datetime.max.time(),
             )
             .astimezone(pytz.utc)
@@ -38,3 +45,13 @@ class ProcurementGroup(models.Model):
                 [("date", "<=", max_date)],
             ]
         )
+
+    def get_global_is_moves_assignation_limited(self):
+        get_param = self.env["ir.config_parameter"].sudo().get_param
+        param = "s_scheduler_assignation_horizon.global_moves_assignation"
+        return str2bool(get_param(param, False))
+
+    def get_global_moves_limit_horizon(self):
+        get_param = self.env["ir.config_parameter"].sudo().get_param
+        param = "s_scheduler_assignation_horizon.stock_scheduler_assignation_horizon"
+        return safe_eval(get_param(param, 0))
