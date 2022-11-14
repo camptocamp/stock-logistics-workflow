5595ed27789c2586d4381a30d7241507eee40d8f
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2022-11-14 15:24:17 +0200

    stock_scheduler_assignation_horizon: wider settings approuche
    scheduler ususally running in global mode when company is not set
    so settings defined on company level are not applied

--- stock_scheduler_assignation_horizon/models/res_config_settings.py
+++ stock_scheduler_assignation_horizon/models/res_config_settings.py
@@ -3,11 +3,24 @@
 
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
+from odoo.tools.safe_eval import safe_eval
 
 
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
+    global_moves_assignation = fields.Boolean(
+        "Use global Assignation Limit",
+        config_parameter="s_scheduler_assignation_horizon.global_moves_assignation",
+        readonly=False,
+        help="Check this box to allow limit settings " "for each company independent",
+    )
+    global_moves_assignation_horizon = fields.Integer(
+        "Assignation Horizon",
+        config_parameter="s_scheduler_assignation_horizon.stock_scheduler_assignation_horizon",
+        readonly=False,
+        help="Only reserve moves that are scheduled within the specified number of days",
+    )
     is_moves_assignation_limited = fields.Boolean(
         "Scheduler Assignation Limit",
         related="company_id.is_moves_assignation_limited",
@@ -27,3 +40,13 @@ class ResConfigSettings(models.TransientModel):
         for record in self:
             if record.moves_assignation_horizon < 0:
                 raise ValidationError(_("The assignation horizon cannot be negative"))
+
+    def get_global_is_moves_assignation_limited(self):
+        get_param = self.env["ir.config_parameter"].sudo().get_param
+        param = "s_scheduler_assignation_horizon.global_moves_assignation"
+        return safe_eval(get_param(param, False))
+
+    def get_global_moves_limit_horizon(self):
+        get_param = self.env["ir.config_parameter"].sudo().get_param
+        param = "s_scheduler_assignation_horizon.stock_scheduler_assignation_horizon"
+        return safe_eval(get_param(param, 0))
