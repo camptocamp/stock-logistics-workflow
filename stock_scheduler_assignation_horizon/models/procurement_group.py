ed9253f85375d9cc4016c9a2b2032e69dee5689a
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2022-11-17 14:37:30 +0200

    fixup! Update stock_scheduler_assignation_horizon/data/ir_config_parameter.xml

--- stock_scheduler_assignation_horizon/models/procurement_group.py
+++ stock_scheduler_assignation_horizon/models/procurement_group.py
@@ -2,13 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from datetime import datetime, timedelta
+from logging import getLogger
 
 import pytz
 
 from odoo import models
 from odoo.osv import expression
 from odoo.tools.misc import str2bool
-from odoo.tools.safe_eval import safe_eval
+
+_logger = getLogger(__name__)
 
 
 class ProcurementGroup(models.Model):
@@ -16,20 +18,14 @@ class ProcurementGroup(models.Model):
 
     def _get_moves_to_assign_domain(self, company_id):
         domain = super()._get_moves_to_assign_domain(company_id)
-        company = self.env["res.company"].browse(company_id)
         global_limit = self.get_global_is_moves_assignation_limited()
-        if company.is_moves_assignation_limited or global_limit:
-            delay = (
-                company.moves_assignation_horizon
-                or self.get_global_moves_limit_horizon()
-            )
-            domain = self._get_moves_to_assign_domain_assignation_limited(
-                domain, company.partner_id.tz, delay
-            )
+        if global_limit:
+            delay = self.get_global_moves_limit_horizon()
+            domain = self._get_moves_to_assign_domain_assignation_limited(domain, delay)
         return domain
 
-    def _get_moves_to_assign_domain_assignation_limited(self, domain, tz, delay_days):
-        applied_tz = pytz.timezone(tz or "UTC")
+    def _get_moves_to_assign_domain_assignation_limited(self, domain, delay_days):
+        applied_tz = pytz.timezone("UTC")
         max_date = (
             datetime.combine(
                 datetime.now(applied_tz) + timedelta(days=delay_days),
@@ -47,10 +43,21 @@ class ProcurementGroup(models.Model):
 
     def get_global_is_moves_assignation_limited(self):
         get_param = self.env["ir.config_parameter"].sudo().get_param
-        param = "s_scheduler_assignation_horizon.stock_horizon_move_assignation"
+        param = "stock_scheduler_assignation_horizon.stock_horizon_move_assignation"
         return str2bool(get_param(param, False))
 
     def get_global_moves_limit_horizon(self):
         get_param = self.env["ir.config_parameter"].sudo().get_param
-        param = "s_scheduler_assignation_horizon.stock_horizon_move_assignation_limit"
-        return safe_eval(get_param(param, 0))
+        param = (
+            "stock_scheduler_assignation_horizon.stock_horizon_move_assignation_limit"
+        )
+        try:
+            param_value = get_param(param, 0)
+            limit = int(param_value)
+        except ValueError:
+            limit = 0
+            _logger.warning(
+                "Wrong value for ir.config_parameter %s: %s . Using %s as default value"
+                % (param, param_value, limit)
+            )
+        return limit
