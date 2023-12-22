4cccd2488c3c94cd2e8efa2a69d778fa815c83b9
Mmequignon <matthieu@fwzte.xyz>
2023-12-22 13:05:00 +0100

    Add sale_stock_mto_as_mts_orderpoint_product_variant

---
+++ sale_stock_mto_as_mts_orderpoint_product_variant/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Sale Stock MTO as MTS Orderpoint Product Variant",
+    "version": "14.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Operations/Inventory/Delivery",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["mmequignon"],
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "sale_stock_mto_as_mts_orderpoint",
+        "mto_route_product_variant",
+    ],
+}
