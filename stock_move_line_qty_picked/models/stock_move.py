dce0fffa46bb126630de759ee18c22c7ffec1e31
Akim Juillerat <akim.juillerat@camptocamp.com>
2025-07-21 18:09:09 +0200

    fixup! Create module stock_move_line_qty_picked

--- stock_move_line_qty_picked/models/stock_move.py
+++ stock_move_line_qty_picked/models/stock_move.py
@@ -9,13 +9,8 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     def _get_picked_quantity(self):
-        if self.picked and any(ml.qty_picked for ml in self.move_line_ids):
-            picked_qty = 0
-            for ml in self.move_line_ids:
-                picked_qty += ml.product_uom_id._compute_quantity(
-                    ml.qty_picked, self.product_uom, round=False
-                )
-            return picked_qty
+        if self._ml_has_qty_picked():
+            return self._sum_ml_qty_picked()
         return super()._get_picked_quantity()
 
     @api.depends("move_line_ids.picked", "move_line_ids.qty_picked")
@@ -25,6 +20,8 @@ class StockMove(models.Model):
             if move.picked and any(ml.qty_picked for ml in move.move_line_ids):
                 picked_moves_ids.append(move.id)
         picked_moves_ids = self.browse(picked_moves_ids)
+
+        # Copied from odoo and adapted for qty_picked
         data = self.env["stock.move.line"]._read_group(
             [("id", "in", picked_moves_ids.move_line_ids.ids)],
             ["move_id", "product_uom_id"],
@@ -42,11 +39,19 @@ class StockMove(models.Model):
         return super(StockMove, not_picked_moves)._compute_quantity()
 
     def _quantity_sml(self):
-        if self.picked and any(ml.qty_picked for ml in self.move_line_ids):
-            quantity = 0
-            for move_line in self.move_line_ids:
-                quantity += move_line.product_uom_id._compute_quantity(
-                    move_line.qty_picked, self.product_uom, round=False
-                )
-            return quantity
+        if self._ml_has_qty_picked():
+            return self._sum_ml_qty_picked()
         return super()._quantity_sml()
+
+    def _ml_has_qty_picked(self):
+        self.ensure_one()
+        return self.picked and any(ml.qty_picked for ml in self.move_line_ids)
+
+    def _sum_ml_qty_picked(self):
+        self.ensure_one()
+        quantity = 0
+        for move_line in self.move_line_ids:
+            quantity += move_line.product_uom_id._compute_quantity(
+                move_line.qty_picked, self.product_uom, round=False
+            )
+        return quantity
